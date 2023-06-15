ad24214b68f7059c16639c567bbef070678ee764
08-Ahlaqul-Karimah <83933498+08-Ahlaqul-Karimah@users.noreply.github.com>
2023-06-15 21:22:04 +0700

    Update index.py

--- index.py
+++ index.py
@@ -7,7 +7,6 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 # import warnings
